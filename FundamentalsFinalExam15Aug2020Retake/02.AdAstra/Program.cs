258318fd599353b0945dfd46ef43b44461e449f5
Mironov <mironov5712s@gmail.com>
2020-08-27 14:08:53 +0300

    status

---
+++ FundamentalsFinalExam15Aug2020Retake/02.AdAstra/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FoodData> foodList = new List<FoodData>();
+            string pattern = @"(\#|\|)(?<name>[a-zA-Z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<calory>\d+)\1";
+            string readLine = Console.ReadLine();
+            MatchCollection getMatches = Regex.Matches(readLine, pattern);
+            if (getMatches.Count > 0)
+            {
+                for (int i = 0; i < getMatches.Count; i++)
+                {
+                    string getName = getMatches[i].Groups["name"].Value;
+                    string getDate = getMatches[i].Groups["date"].Value;
+                    int getCalory = int.Parse(getMatches[i].Groups["calory"].Value);
+                    foodList.Add(new FoodData(getName, getDate, getCalory));
+                }
+            }
+            int sumCalory = foodList.Sum(x => x.CaloryValue);
+            int daysToLive = sumCalory / 2000;
+            Console.WriteLine($"You have food to last you for: {daysToLive} days!");
+            foreach (var item in foodList)
+            {
+                Console.WriteLine($"Item: {item.NameFood}, Best before: {item.DateInfo}, Nutrition: {item.CaloryValue}");
+            }
+        }
+    }
+    public class FoodData
+    {
+        public string NameFood { get; set; }
+        public string DateInfo { get; set; }
+        public int CaloryValue { get; set; }
+        public FoodData(string nameFood, string dateInfo, int caloryValue)
+        {
+            this.NameFood = nameFood;
+            this.DateInfo = dateInfo;
+            this.CaloryValue = caloryValue;
+        }
+    }
+}
---
+++ FundamentalsFinalExam15Aug2020Retake/03.ThePianis/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _03.ThePianis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Composers> musicData = new Dictionary<string, Composers>();
+            int numLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numLines; i++)
+            {
+                string[] readInfo = Console.ReadLine().Split("|");
+                string pieceName = readInfo[0];
+                string composerName = readInfo[1];
+                string keyName = readInfo[2];
+                if (!musicData.ContainsKey(pieceName))
+                    musicData.Add(pieceName, new Composers(composerName, keyName));
+            }
+            string instruction = "";
+            while((instruction = Console.ReadLine()) != "Stop")
+            {
+                string[] commandParts = instruction.Split("|");
+                string majorCommand = commandParts[0];
+                if (majorCommand == "Add")
+                {
+                    string getPiece = commandParts[1];
+                    string getComposer = commandParts[2];
+                    string getKey = commandParts[3];
+                    if (!musicData.ContainsKey(getPiece))
+                    {
+                        musicData.Add(getPiece, new Composers(getComposer, getKey));
+                        Console.WriteLine($"{getPiece} by {getComposer} in {getKey} added to the collection!");
+                    }
+                    else
+                        Console.WriteLine($"{getPiece} is already in the collection!");
+                }
+                else if (majorCommand == "Remove")
+                {
+                    string pieceRemove = commandParts[1];
+                    if (musicData.ContainsKey(pieceRemove))
+                    {
+                        musicData.Remove(pieceRemove);
+                        Console.WriteLine($"Successfully removed {pieceRemove}!");
+                    }
+                    else
+                        Console.WriteLine($"Invalid operation! {pieceRemove} does not exist in the collection.");
+                }
+                else if (majorCommand == "ChangeKey")
+                {
+                    string pieceChange = commandParts[1];
+                    string keyChange = commandParts[2];
+                    if (musicData.ContainsKey(pieceChange))
+                    {
+                        musicData[pieceChange].KeyType = keyChange;
+                        Console.WriteLine($"Changed the key of {pieceChange} to {keyChange}!");
+                    }
+                    else
+                        Console.WriteLine($"Invalid operation! {pieceChange} does not exist in the collection.");
+                }
+            }
+            foreach (var kvp in musicData.OrderBy(x => x.Key).ThenBy(y => y.Value.NameComposer))
+            {
+                Console.WriteLine($"{kvp.Key} -> Composer: {kvp.Value.NameComposer}, Key: {kvp.Value.KeyType}");
+            }
+        }
+    }
+    public class Composers
+    {
+        public string NameComposer { get; set; }
+        public string KeyType { get; set; }
+        public Composers(string nameComposer, string keyType)
+        {
+            this.NameComposer = nameComposer;
+            this.KeyType = keyType;
+        }
+    }
+}
