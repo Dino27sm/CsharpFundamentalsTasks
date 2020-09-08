7abef35818bc91d57cc2c1261d20d59573ed310c
Mironov <mironov5712s@gmail.com>
2020-09-08 20:22:22 +0300

    Create Program.cs

---
+++ Fundament26June2020ObjectsAndClassesMoreExercise/02.OldestFamilyMemberM1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OldestFamilyMemberM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            int numEntries = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numEntries; i++)
+            {
+                string[] lineEnter = Console.ReadLine().Split().ToArray();
+                Person newPerson = new Person(lineEnter[0], int.Parse(lineEnter[1]));
+                personList.Add(newPerson);
+            }
+            Person memberToPrint = personList.OrderByDescending(x => x.Age).First();
+            Console.WriteLine($"{memberToPrint.Name} {memberToPrint.Age}");
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string personName, int personAge)
+        {
+            this.Name = personName;
+            this.Age = personAge;
+        }
+    }
+}
