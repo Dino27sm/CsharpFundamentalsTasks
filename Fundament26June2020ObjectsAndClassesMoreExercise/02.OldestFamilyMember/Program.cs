56855d9ee1a82179997c2a3688ebfc20295d9d14
Mironov <mironov5712s@gmail.com>
2020-09-08 20:21:27 +0300

    Create Program.cs

---
+++ Fundament26June2020ObjectsAndClassesMoreExercise/02.OldestFamilyMember/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> familiData = new Dictionary<string, int>();
+            int numLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numLines; i++)
+            {
+                string[] enterLines = Console.ReadLine().Split().ToArray();
+                string name = enterLines[0];
+                int age = int.Parse(enterLines[1]);
+                if (!familiData.ContainsKey(name))
+                    familiData.Add(name, age);
+            }
+            KeyValuePair<string, int> printOut = familiData.OrderByDescending(x => x.Value).First();
+            //var printOut = familiData.OrderByDescending(x => x.Value).First();
+            Console.WriteLine($"{printOut.Key} {printOut.Value}");
+        }
+    }
+}
