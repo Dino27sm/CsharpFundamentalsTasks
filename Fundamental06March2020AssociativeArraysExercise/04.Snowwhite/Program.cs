45ee91ba882eb718d710941610a156764f5c826b
Mironov <mironov5712s@gmail.com>
2020-09-15 19:52:56 +0300

    Create Program.cs

---
+++ Fundamental06March2020AssociativeArraysExercise/04.Snowwhite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _04.Snowwhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dwarfsData = new Dictionary<string, int>();
+            string inpText = "";
+            while((inpText = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] commandLine = inpText.Split(" <:> ");
+                string name = commandLine[0];
+                string colour = commandLine[1];
+                string nameColour = string.Join(" ", name, colour);
+                int physics = int.Parse(commandLine[2]);
+                if (!dwarfsData.ContainsKey(nameColour))
+                    dwarfsData.Add(nameColour, physics);
+                else
+                {
+                    if (dwarfsData[nameColour] < physics)
+                        dwarfsData[nameColour] = physics;
+                }
+            }
+            foreach (var kvp in dwarfsData.OrderByDescending(x => x.Value)
+                .ThenByDescending(x => dwarfsData.Keys.Count(y => y.Split()[1] == x.Key.Split()[1])))
+            {
+                Console.WriteLine($"({kvp.Key.Split()[1]}) {kvp.Key.Split()[0]} <-> {kvp.Value}");
+            }
+        }
+    }
+}
