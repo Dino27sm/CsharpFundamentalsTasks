7007f37cabc1c7420faf467e79bfb2e1a7a21770
Mironov <mironov5712s@gmail.com>
2020-09-23 19:39:08 +0300

    Create Program.cs

---
+++ Fundamental17July2020TextProcessingMoreExercise/03.TreasureFinder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.TreasureFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternCheck = @".*[&](?<name>[\w]+)[&].*[<](?<place>.*)[>].*";
+            int[] keyNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string inpText = "";
+            while((inpText = Console.ReadLine()) != "find")
+            {
+                char[] charText = inpText.ToCharArray();
+                int indexKey = 0;
+                for (int i = 0; i < charText.Length; i++)
+                {
+                    indexKey = i % keyNum.Length;
+                    charText[i] = (char)(inpText[i] - keyNum[indexKey]);
+                }
+                string dataText = string.Join("",charText);
+                Regex regObject = new Regex(patternCheck);
+                Match getInfo = regObject.Match(dataText);
+                if (getInfo.Success)
+                    Console.WriteLine($"Found {getInfo.Groups["name"]} at {getInfo.Groups["place"]}");
+            }
+        }
+    }
+}
