8b40576e00fd96b6ec7fb743624e73e855380d22
Mironov <mironov5712s@gmail.com>
2020-10-01 12:19:12 +0300

    Create Program.cs

---
+++ Fundament19June2020OListsExerciseAndMoreExercise/06.CardsGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CardsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (firstDeck.Count > 0 && secondDeck.Count > 0)
+            {
+                if (firstDeck[0] > secondDeck[0])
+                {
+                    int temp1 = firstDeck[0];
+                    int temp2 = secondDeck[0];
+                    firstDeck.Add(temp1);
+                    firstDeck.Add(temp2);
+                    secondDeck.RemoveAt(0);
+                    firstDeck.RemoveAt(0);
+                }
+                else if(firstDeck[0] < secondDeck[0])
+                {
+                    int temp1 = secondDeck[0];
+                    int temp2 = firstDeck[0];
+                    secondDeck.Add(temp1);
+                    secondDeck.Add(temp2);
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                }
+                else if (firstDeck[0] == secondDeck[0])
+                {
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                }
+            }
+            if (firstDeck.Count > 0 && secondDeck.Count <= 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstDeck.Sum()}");
+            }
+            else if (firstDeck.Count <= 0 && secondDeck.Count > 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondDeck.Sum()}");
+            }
+        }
+    }
+}
