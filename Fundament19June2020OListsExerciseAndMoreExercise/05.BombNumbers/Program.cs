0dae22bdbbc34dfd7ce9773dc5a5f34eaf1c6604
Mironov <mironov5712s@gmail.com>
2020-10-01 12:18:00 +0300

    Create Program.cs

---
+++ Fundament19June2020OListsExerciseAndMoreExercise/05.BombNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inpList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> bombInfo = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int listLength = inpList.Count;
+            int counter = 0;
+            while(counter < inpList.Count)
+            {
+                if (inpList[counter] == bombInfo[0])
+                {
+                    int leftIndex = 0;
+                    int rightIndex = inpList.Count - 1;
+                    if (counter - bombInfo[1] > 0) leftIndex = counter - bombInfo[1];
+                    if (counter + bombInfo[1] < inpList.Count) rightIndex = counter + bombInfo[1];
+                    int numToRemove = (rightIndex - leftIndex) + 1;
+                    inpList.RemoveRange(leftIndex, numToRemove);
+                    counter = 0;
+                }
+                else counter++;
+            }
+            Console.WriteLine(inpList.Sum());
+        }
+    }
+}
