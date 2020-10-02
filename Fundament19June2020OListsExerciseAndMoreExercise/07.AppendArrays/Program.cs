c20865bc3b262357cbe07f9aa4fd228800426c16
Mironov <mironov5712s@gmail.com>
2020-10-02 14:25:25 +0300

    Create Program.cs

---
+++ Fundament19June2020OListsExerciseAndMoreExercise/07.AppendArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inpList = Console.ReadLine()
+                .Split('|').ToList();
+            for (int i = 0; i < inpList.Count / 2; i++)
+            {
+                string temp = inpList[i];
+                inpList[i] = inpList[inpList.Count - 1 - i];
+                inpList[inpList.Count - 1 - i] = temp;
+            }
+            string txtOut = string.Join(" ", inpList);
+            string[] txtPrint = txtOut.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Console.WriteLine(string.Join(" ", txtPrint));
+        }
+    }
+}
