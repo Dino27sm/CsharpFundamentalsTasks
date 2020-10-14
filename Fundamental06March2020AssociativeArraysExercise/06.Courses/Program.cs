550d18dd2212f742a4c8e74fd1eb750a29c32296
Mironov <mironov5712s@gmail.com>
2020-10-14 19:09:02 +0300

    Create Program.cs

---
+++ Fundamental06March2020AssociativeArraysExercise/06.Courses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> studentInfo = new Dictionary<string, List<string>>();
+            string textEnter;
+            while((textEnter = Console.ReadLine()) != "end")
+            {
+                string[] inpText = textEnter
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string course = inpText[0];
+                string student = inpText[1];
+                if (!studentInfo.ContainsKey(course))
+                {
+                    studentInfo[course] = new List<string>();
+                }
+                studentInfo[course].Add(student);
+            }
+            foreach (var kvp in studentInfo.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                foreach (var item in kvp.Value.OrderBy(z => z))
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
