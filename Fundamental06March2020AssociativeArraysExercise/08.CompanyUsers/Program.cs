9c2b94491ff402e9f7e85bf425b2ad9e817b0ab5
Mironov <mironov5712s@gmail.com>
2020-10-17 14:14:55 +0300

    Create Program.cs

---
+++ Fundamental06March2020AssociativeArraysExercise/08.CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, List<string>> firmInfo = new SortedDictionary<string, List<string>>();
+            string inpText;
+            while ((inpText = Console.ReadLine()) != "End")
+            {
+                string[] currentLine = inpText.Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string companyName = currentLine[0];
+                string employeeId = currentLine[1];
+
+                if (!firmInfo.ContainsKey(companyName))
+                {
+                    firmInfo.Add(companyName, new List<string>());
+                    firmInfo[companyName].Add(employeeId);
+                }
+                else
+                {
+                    if (!firmInfo[companyName].Contains(employeeId))
+                    {
+                        firmInfo[companyName].Add(employeeId);
+                    }
+                }
+            }
+            foreach (var kvp in firmInfo)
+            {
+                Console.WriteLine($"{kvp.Key}");
+                foreach (string item in kvp.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
