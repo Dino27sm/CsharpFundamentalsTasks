7b68da3d8df9e71e2b23736bc7f48c927d121e56
Mironov <mironov5712s@gmail.com>
2021-08-01 11:17:21 +0300

    Create Program.cs

---
+++ Fundamentals-17Jan2020-Conditional Statements-MoreExercise/01.SortNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arayNum = new int[3];
+            for (int i = 0; i <= 2; i++)
+            {
+                arayNum[i] = int.Parse(Console.ReadLine());
+            }
+            int bufferNum;
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int k = i; k <= 2; k++)
+                {
+                    if (arayNum[i] < arayNum[k])
+                    {
+                        bufferNum = arayNum[k];
+                        arayNum[k] = arayNum[i];
+                        arayNum[i] = bufferNum;
+                    }
+                }
+                Console.WriteLine(arayNum[i]);
+            }
+        }
+    }
+}
