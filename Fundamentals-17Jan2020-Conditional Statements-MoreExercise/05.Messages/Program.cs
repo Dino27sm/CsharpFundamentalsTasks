bf6342e21cc0b5309e62cf13b87c59870721817a
Mironov <mironov5712s@gmail.com>
2021-08-02 11:31:49 +0300

    Create Program.cs

---
+++ Fundamentals-17Jan2020-Conditional Statements-MoreExercise/05.Messages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outTxt = "";
+            int massageLeng = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= massageLeng; i++)
+            {
+                string inpNums = Console.ReadLine();
+                int strLength = inpNums.Length;
+                int numValue = int.Parse(inpNums[0].ToString());
+                if (numValue != 0)
+                {
+                    int offcet = (numValue - 2) * 3;
+                    if (numValue == 8 || numValue == 9) offcet++;
+                    int symbolIndex = offcet + strLength - 1;
+                    outTxt += (char)(97 + symbolIndex);
+                }
+                else outTxt += (char)32;
+            }
+            Console.WriteLine(outTxt);
+        }
+    }
+}
