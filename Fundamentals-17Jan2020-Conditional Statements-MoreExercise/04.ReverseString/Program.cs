6f9007dd08cf2eda03c86e5ddfa4cfa9215edf03
Mironov <mironov5712s@gmail.com>
2021-08-02 11:31:05 +0300

    Create Program.cs

---
+++ Fundamentals-17Jan2020-Conditional Statements-MoreExercise/04.ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpTxt = Console.ReadLine();
+            int inpTxtLength = inpTxt.Length;
+            for (int i = inpTxtLength - 1; i >= 0; i--)
+            {
+                Console.Write(inpTxt[i]);
+            }
+        }
+    }
+}
