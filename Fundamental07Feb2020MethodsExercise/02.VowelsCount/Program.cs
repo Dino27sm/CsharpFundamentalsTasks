aaa6319d3ed2f050badb11c95883c1795678ca48
Mironov <mironov5712s@gmail.com>
2021-08-05 09:13:55 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/02.VowelsCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpTxt = Console.ReadLine();
+                //string inpTxt2 = Console.ReadLine().ToLower();    //Converts all big char to small
+            Console.WriteLine(VowelsCount(inpTxt));
+        }
+        static int VowelsCount(string text)
+        {
+            int sumVowels = 0;
+            int txtLength = text.Length;
+            for (int i = 0; i < txtLength; i++)
+            {
+                switch (text[i])
+                {
+                    case 'A':
+                    case 'a':
+                    case 'E':
+                    case 'e':
+                    case 'I':
+                    case 'i':
+                    case 'O':
+                    case 'o':
+                    case 'U':
+                    case 'u':
+                        sumVowels++;
+                        break;
+                }
+            }
+            return sumVowels;
+        }
+    }
+}
