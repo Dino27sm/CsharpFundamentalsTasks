1312869c1330179525e2411921c814f5c3f2d7a9
Mironov <mironov5712s@gmail.com>
2021-08-05 09:14:42 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/03.CharactersRange/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.CharactersRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbolOne = Console.ReadLine();
+            string symbolTwo = Console.ReadLine();
+            CharInRange(symbolOne[0], symbolTwo[0]);
+        }
+
+        static void CharInRange(char firstChar, char secondCHar)
+        {
+            for (int i = Math.Min(firstChar, secondCHar) + 1; i < Math.Max(firstChar, secondCHar); i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
