95541b79b39f7d6d4a4405d410d40f0f473cdb8b
Mironov <mironov5712s@gmail.com>
2021-08-05 09:11:55 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/01.SmallestThreeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SmallestThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int minNum = SmollestOfThreeNum(firstNum, secondNum, thirdNum);
+            Console.WriteLine(minNum);
+        }
+        static int SmollestOfThreeNum(int a, int b, int c)
+        {
+            int smollestNum = Math.Min(a, Math.Min(b, c));
+            return smollestNum;
+        }
+    }
+}
