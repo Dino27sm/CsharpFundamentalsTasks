1e7fd1691fa54634f584a8c71ca7112e107ccdb4
Mironov <mironov5712s@gmail.com>
2021-08-06 17:28:55 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/07.NxNtoMatrix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07.NxNtoMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            NxNprintMatrix(matrixSize);
+        }
+
+        private static void NxNprintMatrix(int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int k = 1; k <= size; k++) Console.Write($"{size} ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
