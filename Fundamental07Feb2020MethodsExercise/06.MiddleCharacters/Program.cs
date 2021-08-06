07e39fc17f83dd7d646f2cef34360578d9b21969
Mironov <mironov5712s@gmail.com>
2021-08-06 17:27:47 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpTxt = Console.ReadLine();
+            PrintMidChar(inpTxt);
+
+        }
+        static void PrintMidChar(string text)
+        {
+            int lengthText = text.Length;
+            int halfLength = lengthText / 2;
+            if (lengthText % 2 == 0) Console.WriteLine($"{text[halfLength - 1]}{text[halfLength]}");
+            else Console.WriteLine($"{text[halfLength]}");
+        }
+    }
+}
