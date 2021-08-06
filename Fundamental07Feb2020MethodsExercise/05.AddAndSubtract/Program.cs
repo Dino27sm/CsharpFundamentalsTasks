3cc29547aa6829a0585ded7dacaf3fb8dc797fb9
Mironov <mironov5712s@gmail.com>
2021-08-06 17:27:02 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int result = SubtractTwoNumb(SumTwoNum(num1, num2), num3);
+            Console.WriteLine(result);
+        }
+
+        static int SumTwoNum(int addend1, int addend2)
+        {
+            return (addend1 + addend2);
+        }
+
+        static int SubtractTwoNumb(int deducted, int deductor)
+        {
+            return (deducted - deductor);
+        }
+    }
+}
