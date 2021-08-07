29eed8bda066ace55c5e0a306420938ea5145abd
Mironov <mironov5712s@gmail.com>
2021-08-07 07:43:31 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/10.TopNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10.TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inpIntNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inpIntNum; i++)
+            {
+                if (IsTopNumber(i)) Console.WriteLine(i);
+            }
+        }
+        //-------------------------------------------------------------
+        static bool IsTopNumber(int num)
+        {
+            bool oddDigit = false;
+            int sumDigits = 0;
+            while(num > 0)
+            {
+                int digit = num % 10;
+                if (digit % 2 == 1) oddDigit = true;
+                sumDigits += digit;
+                num /= 10;
+            }
+            return (sumDigits % 8 == 0 && oddDigit);
+        }
+    }
+}
