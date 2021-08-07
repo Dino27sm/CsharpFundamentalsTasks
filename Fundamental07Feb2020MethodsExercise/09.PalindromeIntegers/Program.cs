76e4b3223c8edfe53c31f10ace95f80e324bafd6
Mironov <mironov5712s@gmail.com>
2021-08-07 07:42:30 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpTxt = "";
+            while((inpTxt = Console.ReadLine()) != "END")
+            {
+                if (CheckPalindromeIntegers(inpTxt)) Console.WriteLine("true");
+                else Console.WriteLine("false");
+            }
+        }
+
+        static bool CheckPalindromeIntegers(string inputTxt)
+        {
+            bool flag = true;
+            for (int i = 0; i < inputTxt.Length / 2; i++)
+            {
+                if (inputTxt[i] != inputTxt[inputTxt.Length - 1 - i]) flag = false;
+            }
+            return flag;
+        }
+    }
+}
