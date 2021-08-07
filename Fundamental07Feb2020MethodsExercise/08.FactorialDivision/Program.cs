eb2d46f2979207c2e7a6543c4b7dc11d7eb79b5f
Mironov <mironov5712s@gmail.com>
2021-08-07 07:41:38 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsExercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{1.0 * NumFactorial(firstNum) / NumFactorial(secondNum):F2}");
+        }
+        
+        static long NumFactorial(int num)
+        {
+            long resultOut = 1L;
+            if (num > 0)
+            {
+                for (int i = 1; i <= num; i++) resultOut *= i;
+            }
+            else resultOut = 1L;
+            return resultOut;
+        }
+    }
+}
