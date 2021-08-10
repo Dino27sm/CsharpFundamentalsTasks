93e6dd8a9bec6837cc8f275fadc9eb118dc77ca8
Mironov <mironov5712s@gmail.com>
2021-08-10 09:37:09 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsMoreExercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inpNum = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                inpNum[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(GetProductType(inpNum));
+        }
+
+        private static string GetProductType(double[] arr)
+        {
+            if(arr.Any(x => x == 0))
+                return "zero";
+
+            if(arr.Count(x => x < 0) == 1 || arr.Count(x => x < 0) == 3)
+                return "negative";
+
+            return "positive ";
+        }
+    }
+}
