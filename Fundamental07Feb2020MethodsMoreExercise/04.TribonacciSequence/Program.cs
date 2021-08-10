233538119ad4d7d1fa8e9f5ed16953af1797f60e
Mironov <mironov5712s@gmail.com>
2021-08-10 09:36:24 +0300

    Create Program.cs

---
+++ Fundamental07Feb2020MethodsMoreExercise/04.TribonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<int> tribonacciSequence = new List<int>() { 1};
+
+            while (tribonacciSequence.Count < num)
+            {
+                tribonacciSequence.Add(tribonacciSequence.TakeLast(3).Sum());
+            }
+            Console.WriteLine(string.Join(" ", tribonacciSequence));
+        }
+    }
+}
